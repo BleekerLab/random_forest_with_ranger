5973cd2a265c0c17853fdf508992f98c2f758bb7
Marc Galland <m.galland@uva.nl>
2020-09-02 15:16:25 +0200

    add master_doc value

--- docs/conf.py
+++ docs/conf.py
@@ -27,8 +27,14 @@ author = 'Marc Galland'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'restructuredtext',
+    '.md': 'markdown',
+}
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -37,6 +43,10 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# The document name of the “master” document, that is, the document that contains the root toctree directive. 
+# Default is 'index'.
+master_doc = "index"
 
 
 # -- Options for HTML output -------------------------------------------------
